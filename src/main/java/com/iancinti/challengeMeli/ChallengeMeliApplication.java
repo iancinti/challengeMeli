8b1e900629d693b23a1e378b8356240e1ef0bd97
iancinti <ian.cinti@gmail.com>
2024-05-10 16:33:53 -0300

    Initial commit

---
+++ src/main/java/com/iancinti/challengeMeli/ChallengeMeliApplication.java
@@ -0,0 +1,13 @@
+package com.iancinti.challengeMeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeMeliApplication.class, args);
+	}
+
+}
